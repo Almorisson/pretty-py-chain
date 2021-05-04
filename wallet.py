661bca6c0107ed735262dd03b2a45e1a9d1ebcc3
Mor NDIAYE <mo.ndiaye@ecole-ipssi.net>
2021-05-04 23:05:17 +0200

    [feat]: Wallet class

---
+++ wallet.py
@@ -0,0 +1,11 @@
+from Crypto.PublicKey import RSA
+from Crypto.Random import random
+
+class Wallet:
+    def __init__(self):
+        pass
+
+    def generate_keys(self):
+        pass
+
+print(random)
